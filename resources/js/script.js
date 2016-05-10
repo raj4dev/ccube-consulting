e172292157d964705803419e6b512fd203a2e8a6
rb <rb@email.com>
2016-05-10 11:51:02 -0700

    Play button now shows or hides company name. Will make it show services list later.

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,7 +1,20 @@
+/*Get today's date and display it.*/
 var time = new Date();
 var datestring = new Date().toLocaleDateString('en-GB', {  
     day : 'numeric',
     month : 'short',
     year : 'numeric'
 }).split(' ').join('/');
+
 document.getElementById('clock').innerHTML = datestring;
+
+
+/*Show Company name when services offered button is clicked. Then, hide it
+when the button is clicked again.*/
+var servicesOfferedBtn = document.getElementById('services-offered');
+
+servicesOfferedBtn.onclick = function() {
+    var companyName = document.querySelector("#company-name");
+    companyName.classList.toggle("hidden");
+}
+
